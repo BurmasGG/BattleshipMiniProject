4b4269da7761cbe29a1ac202ce9f020a0fcc03e8
LynzZen <mathias.s.lynge@gmail.com>
2017-11-01 10:20:40 +0100

    A function that draws a window

--- BattleshipMiniProject/Main.cpp
+++ BattleshipMiniProject/Main.cpp
@@ -2,11 +2,49 @@
 #include<WS2tcpip.h>
 #pragma comment (lib, "ws2_32.lib")
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 
 using namespace std;
 
+//For testing SFML
+void drawWindow() {
+	// create the window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	int Size = 10;
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		
+		sf::CircleShape shape(Size);
+		shape.setFillColor(sf::Color(100, 250, 50));
+		shape.setPosition(123,100);
+		sf::Event event;
+		
+		if (Size < 400)
+		Size++;
+		
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		window.draw(shape);
+		// end the current frame
+		window.display();
+	}
+
+}
 void main()
 {
+	//SLET DRAWWINDOW() FOR AT KORE RESTEN AF PROGRAMMET 
+	drawWindow();
+	
 	//initialize winsock
 	WSADATA wsData;
 	WORD ver = MAKEWORD(2, 2); //request version 2,2
